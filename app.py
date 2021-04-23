7d9f876a3de5762c54da80cb54ed46e770a9d661
Craig <craigwilsonmusic@gmail.com>
2021-04-23 20:34:52 +0100

    app.py set up

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+from controllers.country_controller import countries_blueprint
+from controllers.vu_point_controller import vu_points_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(countries_blueprint)
+app.register_blueprint(vu_points_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ repositories/country_repository.py
@@ -0,0 +1,12 @@
+from db.run_sql import run_sql
+
+from models.country import Country
+from models.vu_point import Vu_point
+
+def save(country):
+    sql = "INSERT INTO countries (name, capital, continent, visited) VALUES (%s, %s, %s, %s) RETURNING *"
+    values = [country.name, country.capital, country.continent, country.visited]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    country.id = id 
+    return country
