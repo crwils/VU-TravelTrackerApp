6f692f5ad4aa04f6152f36f46588e3940f246c6b
Craig <craigwilsonmusic@gmail.com>
2021-04-23 19:58:35 +0100

    vu_app.sql file set up with databases

--- db/vu_app.sql
+++ db/vu_app.sql
@@ -2,5 +2,18 @@ DROP TABLE countries;
 DROP TABLE vu_points;
 
 CREATE TABLE countries (
-    
-)
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(255), 
+    capital VARCHAR(255),
+    continent VARCHAR(255),
+    visited BOOLEAN
+);
+
+CREATE TABLE vu_points (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    rating INT,
+    description TEXT, 
+    visited BOOLEAN,
+    country_id INT REFERENCES countries(id) ON DELETE CASCADE
+);
